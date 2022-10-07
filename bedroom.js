1e5607a2e6a8604b6253dc4140e42509efb1ec40
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-10-07 21:01:07 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,31 @@
+var img = "";
+var objectDetector = "";
+
+function preload() {
+    img = loadImage("bedroom.jpg");
+}
+
+function setup() {
+    var canvas = createCanvas(640, 420);
+    canvas.center();
+    //objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    //objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
